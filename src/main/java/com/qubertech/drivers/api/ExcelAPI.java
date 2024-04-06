fcce8f99cd895e88b01f0e1c9612c3adc178a88f
Arun Karthi Mani <arunkarthi.mani@gmail.com>
2024-04-07 01:05:54 +0530

    Updated Filepath

--- src/main/java/com/qubertech/drivers/api/ExcelAPI.java
+++ src/main/java/com/qubertech/drivers/api/ExcelAPI.java
@@ -1,9 +1,13 @@
 package com.qubertech.drivers.api;
 
+import java.io.IOException;
+import java.io.InputStream;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,10 +16,17 @@ public class ExcelAPI {
     @Value("${datasource.path}")
     String path;
     
+    private final ResourceLoader resourceLoader;
+    
+    public ExcelAPI(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+    
     @Bean
-    public Workbook workbook() throws java.io.IOException
-    {
-        java.io.FileInputStream file = new java.io.FileInputStream(path);
-        return WorkbookFactory.create(file);
+    public Workbook workbook() throws IOException {
+        Resource resource = resourceLoader.getResource(path);
+        try (InputStream inputStream = resource.getInputStream()) {
+            return WorkbookFactory.create(inputStream);
+        }
     }
 }
